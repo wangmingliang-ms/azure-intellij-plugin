d49fdf596652fdce33605988a206e3e71d44f08d
GitHub Action <action@github.com>
2021-05-29 15:31:29 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wangmingliangms/azureintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wangmingliangms.azureintellijplugin.services
+
+import com.github.wangmingliangms.azureintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/wangmingliangms/azureintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wangmingliangms/azureintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangmingliangms.azureintellijplugin.services
 
+import com.github.wangmingliangms.azureintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
